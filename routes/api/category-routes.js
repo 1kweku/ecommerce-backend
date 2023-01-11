643dc5f569f84df19c06f7245e3e22cccf927206
Kweku Adarkwa <1kweku@Kwekus-MacBook-Air-2.local>
2023-01-11 17:51:31 -0500

    routes created

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,66 @@
 const router = require('express').Router();
+const Book = require('../../../RUT-VIRT-FSF-PT-08-2022-U-LOLC/13-ORM/01-Activities/08-Stu_Update-Delete/Unsolved/models/Book');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{}]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+ try {
+  const categoryID = Category.findByPk(request.params.id, {
+    include: [{}]
+  })
+  if (!categoryID) {
+    res.status(404).json({message: 'No category found with this ID!'})
+  }
+ } catch (err) {
+  res.status(500).json(err)
+ }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+  const newCategory = await Category.create(req.body);
+  res.status(200).json(newCategory);
+  } catch (err) {
+  res.status(500).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try { const updateCategory = await Category.update({
+    category_name: req.body.category_name
+  },
+  { where: {
+    id: req.params.id
+  }
+}, res.status(200).json()
+)
+} catch (err) {
+  res.status(500).json(err)
+}
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedCategory = await Book.destroy({
+      where: {
+        id: req.params.id
+      } 
+    })
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
